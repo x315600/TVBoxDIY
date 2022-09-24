56f8fd15385a4a233ea586fff9f108ba2a2c32c8
315600 <512687325@qq.com>
2022-09-24 21:36:43 +0800

    Update diy-T.sh

--- diy-T.sh
+++ diy-T.sh
@@ -27,6 +27,8 @@ sed -i 's/TVBox/极影视/g' $CURRENT_DIR/$DIR/app/src/main/res/values/strings.x
 mv $CURRENT_DIR/DIY/app_icon.png $CURRENT_DIR/$DIR/app/src/main/res/drawable/app_icon.png
 #背景修改
 mv $CURRENT_DIR/DIY/app_bg.png $CURRENT_DIR/$DIR/app/src/main/res/drawable/app_bg.png
+#自带源修改
+sed -i 's/app_source"></app_source">http:\/\/123123.xyz\/cat</g' $CURRENT_DIR/$DIR/app/src/main/res/values-zh/strings.xml
 #取消选集全屏
 sed -i 's/if (showPreview \&\& !fullWindows) toggleFullPreview/\/\/if (showPreview \&\& !fullWindows) toggleFullPreview/g' $CURRENT_DIR/$DIR/app/src/main/java/com/github/tvbox/osc/ui/activity/DetailActivity.java
 #缩略图清晰度修改
